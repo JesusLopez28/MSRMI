4aab38b6855a04aa86a884be7249706856c063a8
JesusLopez28 <jesus.lopez280402@gmail.com>
2024-06-04 23:56:01 -0600

    IU commit

--- src/implementacionClienteServidor.java
+++ src/implementacionClienteServidor.java
@@ -1,12 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
 
-public class implementacionClienteServidor extends UnicastRemoteObject implements ClienteServidor, Runnable {
+public class implementacionClienteServidor extends UnicastRemoteObject implements ClienteServidor, Runnable, ActionListener {
     public ServidorPrincipal servidor;
     public String nombre = null;
     public final MergeSort mergeSort;
+    private JTextArea originalTextArea;
+    private JTextArea ordenadoTextArea;
+    private JTextField tamano;
+    private JButton secuencialButton;
+    private JButton forkJoinButton;
+    private JButton executorServiceButton;
+    private JTextField tiempoSecuencial;
+    private JTextField tiempoForkJoin;
+    private JTextField tiempoExecutorService;
+    private JTextField tiempoTotalSecuencial;
+    private JTextField tiempoTotalForkJoin;
+    private JTextField tiempoTotalExecutorService;
 
     public implementacionClienteServidor(String nombre, ServidorPrincipal servidor) throws RemoteException {
         this.nombre = nombre;
@@ -48,6 +64,7 @@ public class implementacionClienteServidor extends UnicastRemoteObject implement
 
     @Override
     public void run() {
+        IUFrame();
         while (true) {
             Scanner s = new Scanner(System.in);
             System.out.println("Digite el tamaño del array: ");
@@ -66,6 +83,120 @@ public class implementacionClienteServidor extends UnicastRemoteObject implement
                 e.printStackTrace();
             }
         }
+    }
+
+    public void IUFrame() {
+        JFrame frame = new JFrame(nombre);
+        frame.setSize(800, 800);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+
+        JPanel panel = new JPanel();
+        panel.setLayout(new BorderLayout());
+        frame.getContentPane().add(panel);
+
+        Panel textAreasPanel = new Panel(new GridLayout(1, 2, 10, 10));
+        panel.add(textAreasPanel, BorderLayout.CENTER);
+
+        originalTextArea = new JTextArea();
+        originalTextArea.setEditable(false);
+        originalTextArea.setLineWrap(true);
+        originalTextArea.setWrapStyleWord(true);
+        JScrollPane originalScrollPane = new JScrollPane(originalTextArea);
+        textAreasPanel.add(originalScrollPane);
+
+        ordenadoTextArea = new JTextArea();
+        ordenadoTextArea.setEditable(false);
+        ordenadoTextArea.setLineWrap(true);
+        ordenadoTextArea.setWrapStyleWord(true);
+        JScrollPane ordenadoScrollPane = new JScrollPane(ordenadoTextArea);
+        textAreasPanel.add(ordenadoScrollPane);
+
+        JPanel inputPanel = new JPanel(new FlowLayout());
+        panel.add(inputPanel, BorderLayout.NORTH);
+
+        JLabel sizeLabel = new JLabel("Tamaño:");
+        inputPanel.add(sizeLabel);
+
+        tamano = new JTextField(10);
+        tamano.setText("0");
+        inputPanel.add(tamano);
+
+        JButton nuevoButton = new JButton("Nuevo");
+        nuevoButton.addActionListener(this);
+        inputPanel.add(nuevoButton);
+
+        secuencialButton = new JButton("Secuencial");
+        secuencialButton.addActionListener(this);
+        inputPanel.add(secuencialButton);
+        secuencialButton.setEnabled(false);
+
+        forkJoinButton = new JButton("ForkJoin");
+        forkJoinButton.addActionListener(this);
+        inputPanel.add(forkJoinButton);
+        forkJoinButton.setEnabled(false);
+
+        executorServiceButton = new JButton("ExecutorService");
+        executorServiceButton.addActionListener(this);
+        inputPanel.add(executorServiceButton);
+        executorServiceButton.setEnabled(false);
+
+        JButton limpiarButton = new JButton("Limpiar");
+        limpiarButton.addActionListener(this);
+        inputPanel.add(limpiarButton);
+
+        JPanel tiempoPanel = new JPanel(new GridLayout(2, 3, 10, 10)); // GridLayout con 2 filas y 3 columnas
+        panel.add(tiempoPanel, BorderLayout.SOUTH);
+
+        JLabel tiempoSecuencialLabel = new JLabel("Proceso Secuencial:");
+        tiempoPanel.add(tiempoSecuencialLabel);
+
+        tiempoSecuencial = new JTextField(10);
+        tiempoSecuencial.setEditable(false);
+        tiempoPanel.add(tiempoSecuencial);
+
+        JLabel tiempoForkJoinLabel = new JLabel("Proceso ForkJoin:");
+        tiempoPanel.add(tiempoForkJoinLabel);
+
+        tiempoForkJoin = new JTextField(10);
+        tiempoForkJoin.setEditable(false);
+        tiempoPanel.add(tiempoForkJoin);
+
+        JLabel tiempoExecutorServiceLabel = new JLabel("Proceso ExecutorService:");
+        tiempoPanel.add(tiempoExecutorServiceLabel);
+
+        tiempoExecutorService = new JTextField(10);
+        tiempoExecutorService.setEditable(false);
+        tiempoPanel.add(tiempoExecutorService);
+
+        JLabel tiempoTotalSecuencialLabel = new JLabel("Total Secuencial:");
+        tiempoPanel.add(tiempoTotalSecuencialLabel);
+
+        tiempoTotalSecuencial = new JTextField(10);
+        tiempoTotalSecuencial.setEditable(false);
+        tiempoPanel.add(tiempoTotalSecuencial);
+
+        JLabel tiempoTotalForkJoinLabel = new JLabel("Total ForkJoin:");
+        tiempoPanel.add(tiempoTotalForkJoinLabel);
+
+        tiempoTotalForkJoin = new JTextField(10);
+        tiempoTotalForkJoin.setEditable(false);
+        tiempoPanel.add(tiempoTotalForkJoin);
+
+        JLabel tiempoTotalExecutorServiceLabel = new JLabel("Total ExecutorService:");
+        tiempoPanel.add(tiempoTotalExecutorServiceLabel);
+
+        tiempoTotalExecutorService = new JTextField(10);
+        tiempoTotalExecutorService.setEditable(false);
+        tiempoPanel.add(tiempoTotalExecutorService);
+
+        frame.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 
 }
